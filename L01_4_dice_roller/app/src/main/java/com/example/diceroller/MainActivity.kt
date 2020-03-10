1d2034132975d07ff7c42f31042e3f0304d87e6e
ashj <www.shoji@gmail.com>
2020-03-10 08:35:20 -0300

    [dice_roller][func] on click button shows random number
    Signed-off-by: ashj <www.shoji@gmail.com>

--- L01_4_dice_roller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ L01_4_dice_roller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,10 +14,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.text = "Let's Roll"
+        //rollButton.text = "Let's Roll"
         rollButton.setOnClickListener{
-            Toast.makeText(this, "Rolled the dice!", Toast.LENGTH_LONG).show()
+            rollDice()
 
         }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        val randomInt = Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
     }
 }
