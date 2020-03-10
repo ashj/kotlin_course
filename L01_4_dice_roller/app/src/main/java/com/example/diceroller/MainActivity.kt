607b1778e5313aa545e6d34b79c31c400b07a356
ashj <www.shoji@gmail.com>
2020-03-10 08:57:32 -0300

    [dice_roller][func] lateinit var on diceImage
    Signed-off-by: ashj <www.shoji@gmail.com>

--- L01_4_dice_roller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ L01_4_dice_roller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -9,6 +9,7 @@ import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +19,8 @@ class MainActivity : AppCompatActivity() {
         //rollButton.text = "Let's Roll"
         rollButton.setOnClickListener{
             rollDice()
-
         }
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
@@ -32,8 +33,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
     }
 }
