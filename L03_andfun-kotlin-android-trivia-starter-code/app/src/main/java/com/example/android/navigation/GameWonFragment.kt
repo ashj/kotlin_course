9ddc790e331cf3df63c818f356e1da7b3e55006c
ashj <www.shoji@gmail.com>
2020-03-13 13:35:07 -0300

    [trivia] share menu - avoid crash if system does not support the MIME type
    Signed-off-by: ashj <www.shoji@gmail.com>

--- L03_andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ L03_andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -47,7 +47,10 @@ class GameWonFragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater?.inflate(R.menu.winner_menu, menu)
+        inflater.inflate(R.menu.winner_menu, menu)
+        if (null == getSharedIntent().resolveActivity(activity!!.packageManager)) {
+            menu.findItem(R.id.share)?.isVisible = false
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
