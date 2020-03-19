b6a5f7eaeb76ebaaf964eff14b40d22fbeceb689
ashj <www.shoji@gmail.com>
2020-03-19 14:29:39 -0300

    [K05]
    Signed-off-by: ashj <www.shoji@gmail.com>

--- K05_essentials/src/com/example/essentials/Pair/BasicBook.kt
+++ K05_essentials/src/com/example/essentials/Pair/BasicBook.kt
@@ -1,2 +1,23 @@
 package com.example.essentials.Pair
 
+class BasicBook (var title: String = "Test book", var author: String = "Authur Wrighter", var year: Int = 1990) {
+
+    fun getBasicInfo(): Pair<String, String> {
+        return title to author
+    }
+
+    fun getAllInfo(): Triple<String, String, Int> {
+        return Triple (title, author, year)
+    }
+}
+
+
+fun main() {
+    var book = BasicBook()
+
+    var pair = book.getBasicInfo()
+    println ("Title: ${pair.first} - Author: ${pair.second}")
+    var triple  = book.getAllInfo()
+    println ("Title: ${triple.first} - Author: ${triple.second} - Year: ${triple.third}")
+
+}
